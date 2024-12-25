9350ff393cad490bd067caae0e6357f6b4e8a595
Hugo GB <hugogaben8.02@gmail.com>
2024-12-25 04:50:30 +0100

    fix build error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -116,11 +116,8 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
   } catch (error) {
     console.log(error);
-
-    return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
 
